0a469cb0b4eacc3104b07e78b0a409f8d7e32d38
Alex Jupiter <alex@alexjupiter.com>
2022-06-25 21:28:09 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+wallet.registerRpcMessageHandler(async (originString, requestObject) => {
+  switch (requestObject.method) {
+    case 'hello':
+      return wallet.request({
+        method: 'snap_confirm',
+        params: [
+          {
+            prompt: `Hello, ${originString}!`,
+            description:
+              'This custom confirmation is just for display purposes.',
+            textAreaContent:
+              'Hi Alex, I am a Snap',
+          },
+        ],
+      });
+    default:
+      throw new Error('Method not found.');
+  }
+});
